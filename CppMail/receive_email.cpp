80394526fc6a48c2dbba93bc68df7866c613c501
404-error-404 <1791781644@qq.com>
2020-03-11 15:24:53 +0800

    能和服务器进行通信了，但还是比较佛系

--- CppMail/receive_email.cpp
+++ CppMail/receive_email.cpp
@@ -14,9 +14,10 @@ int receive_eamil()
 	WSADATA wsd; //WSADATA变量  
 	SOCKET sHost; //服务器套接字  
 	SOCKADDR_IN servAddr; //服务器地址  
-	char buf[BUF_SIZE]; //接收数据缓冲区  
+	char bufSend[BUF_SIZE]; //接收数据缓冲区  
 	char bufRecv[BUF_SIZE];
 	int retVal; //返回值  
+	int iii = 0;	// 测试
 				//初始化套结字动态库  
 	if (WSAStartup(MAKEWORD(2, 2), &wsd) != 0)
 	{
@@ -33,7 +34,7 @@ int receive_eamil()
 	}
 	//设置服务器地址  
 	servAddr.sin_family = AF_INET;
-	servAddr.sin_addr.s_addr = inet_addr("58.251.106.181");
+	servAddr.sin_addr.s_addr = inet_addr("123.126.97.1");
 	servAddr.sin_port = htons((short)25);
 	int nServAddlen = sizeof(servAddr);
 	//连接服务器  
@@ -45,13 +46,17 @@ int receive_eamil()
 		WSACleanup(); //释放套接字资源  
 		return -1;
 	}
+	cout << "服务器连接成功" << endl;
 	while (true)
 	{
 		//向服务器发送数据  
-		ZeroMemory(buf, BUF_SIZE);
+		ZeroMemory(bufSend, BUF_SIZE);
 		cout << " 向服务器发送数据:  ";
-		cin >> buf;
-		retVal = send(sHost, buf, strlen(buf), 0);
+		cin.getline(bufSend, BUF_SIZE - 1);
+		// string buf_temp;
+		// cin >> buf_temp;
+		// strcpy_s(buf, buf_temp.c_str());
+		retVal = send(sHost, bufSend, strlen(bufSend), 0);
 		if (SOCKET_ERROR == retVal)
 		{
 			cout << "send failed!" << endl;
@@ -59,7 +64,7 @@ int receive_eamil()
 			WSACleanup(); //释放套接字资源  
 			return -1;
 		}
-		//RecvLine(sHost, bufRecv);  
+		// RecvLine(sHost, bufRecv);  
 		ZeroMemory(bufRecv, BUF_SIZE);
 		recv(sHost, bufRecv, BUF_SIZE, 0); // 接收服务器端的数据， 只接收5个字符  
 		cout << endl << "从服务器接收数据：" << bufRecv;
